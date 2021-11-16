05735c065d77bd1d9a1d4e7bdea33a1cfdcdd733
baptiste.crespy@gmail.com <baptiste.crespy@gmail.com>
2021-11-16 22:34:02 +0200

    ✨ moved graphiql

--- src/routes/graphiql.ts
+++ src/routes/graphiql.ts
@@ -3,6 +3,6 @@ import path from "path"
 
 export function routeGraphiql(app: Express) {
   app.get('/graphiql', function(req, res) {
-    res.sendFile(path.join(__dirname, 'graphiql.html'))
+    res.sendFile(path.join(__dirname, '../../public/graphiql.html'))
   })
 }
