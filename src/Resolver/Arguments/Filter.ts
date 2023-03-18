d61a20f1ad7624a021118b1a65086681a7e8faff
ciphrd <crespy.b@gmail.com>
2023-03-18 18:43:16 +0100

    add randomTopGenerativeToken query

--- src/Resolver/Arguments/Filter.ts
+++ src/Resolver/Arguments/Filter.ts
@@ -1,9 +1,15 @@
-import { createUnionType, Field, InputType, ObjectType, registerEnumType } from "type-graphql"
+import {
+  createUnionType,
+  Field,
+  InputType,
+  ObjectType,
+  registerEnumType,
+} from "type-graphql"
 
 export enum FeatureType {
-  BOOLEAN              = "BOOLEAN",
-  STRING               = "STRING",
-  NUMBER               = "NUMBER",
+  BOOLEAN = "BOOLEAN",
+  STRING = "STRING",
+  NUMBER = "NUMBER",
 }
 
 registerEnumType(FeatureType, {
@@ -17,16 +23,17 @@ export class FeatureFilter {
   name: string
 
   @Field(type => [String], {
-    description: "The serialized value, because GraphQL only supports a single type for inputs"
+    description:
+      "The serialized value, because GraphQL only supports a single type for inputs",
   })
   values: string[]
 
   @Field(type => FeatureType, {
-    description: "GraphQL doesn't support multiple types as input, so this fields indicates how to process the value"
+    description:
+      "GraphQL doesn't support multiple types as input, so this fields indicates how to process the value",
   })
   type: FeatureType
 }
-
 
 /**
  * Input which can be run for a site-wide search
@@ -34,7 +41,7 @@ export class FeatureFilter {
 @InputType()
 export class SearchFilters {
   @Field({
-    description: "The string which will be run for the search."
+    description: "The string which will be run for the search.",
   })
   searchQuery_eq: string
 }
