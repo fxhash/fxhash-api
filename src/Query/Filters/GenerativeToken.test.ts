3e58afe839f11963c6678fc302214acfca7b2f3e
louis <louisholley@me.com>
2023-05-12 15:19:24 +0100

    exclude only reserves left from ongoing mint progress filter

--- src/Query/Filters/GenerativeToken.test.ts
+++ src/Query/Filters/GenerativeToken.test.ts
@@ -26,6 +26,104 @@ afterEach(cleanup)
 
 describe("generativeQueryFilter", () => {
   describe("when filtering on mintProgress_eq", () => {
+    describe("ONGOING", () => {
+      const filters = {
+        mintProgress_eq: "ONGOING",
+      }
+
+      const tokenIds = {
+        ZERO_BALANCE: 0,
+        POSITIVE_BALANCE: 1,
+        POSITIVE_BALANCE_WITH_RESERVES: 2,
+        ALL_RESERVED: 3,
+      }
+
+      beforeEach(async () => {
+        // Clean up the database before each test
+        await cleanup()
+
+        // create a generative token with 0 balance
+        await generativeTokenFactory(
+          tokenIds.ZERO_BALANCE,
+          GenerativeTokenVersion.V3,
+          {
+            supply: 100,
+            balance: 0,
+          }
+        )
+
+        // create a generative token with > 0 balance
+        await generativeTokenFactory(
+          tokenIds.POSITIVE_BALANCE,
+          GenerativeTokenVersion.V3,
+          {
+            supply: 100,
+            balance: 50,
+          }
+        )
+
+        // create a generative token with > 0 balance and some reserves
+        await generativeTokenFactory(
+          tokenIds.POSITIVE_BALANCE_WITH_RESERVES,
+          GenerativeTokenVersion.V3,
+          {
+            supply: 100,
+            balance: 50,
+          }
+        )
+        await reserveFactory(tokenIds.POSITIVE_BALANCE_WITH_RESERVES, {
+          amount: 25,
+        })
+
+        // create a generative token with all balance as reserves
+        await generativeTokenFactory(
+          tokenIds.ALL_RESERVED,
+          GenerativeTokenVersion.V3,
+          {
+            supply: 100,
+            balance: 50,
+          }
+        )
+        await reserveFactory(tokenIds.ALL_RESERVED, {
+          amount: 50,
+        })
+      })
+
+      it("should return tokens with > 0 balance and balance - reserves > 0", async () => {
+        let query = GenerativeToken.createQueryBuilder("token").select()
+        query = await generativeQueryFilter(query, filters)
+        const tokens = await query.getMany()
+
+        const filteredTokens = tokens.filter(
+          token =>
+            token.id === tokenIds.POSITIVE_BALANCE ||
+            token.id === tokenIds.POSITIVE_BALANCE_WITH_RESERVES
+        )
+
+        expect(filteredTokens.length).toEqual(2)
+      })
+
+      it("should exclude tokens with 0 balance", async () => {
+        let query = GenerativeToken.createQueryBuilder("token").select()
+        query = await generativeQueryFilter(query, filters)
+        const tokens = await query.getMany()
+
+        expect(
+          tokens.some(token => token.id === tokenIds.ZERO_BALANCE)
+        ).toBeFalsy()
+      })
+
+      it("should exclude tokens with all balance as reserves", async () => {
+        let query = GenerativeToken.createQueryBuilder("token").select()
+        query = await generativeQueryFilter(query, filters)
+        const tokens = await query.getMany()
+
+        expect(
+          tokens.some(token => token.id === tokenIds.ALL_RESERVED)
+        ).toBeFalsy()
+      })
+    })
+
     describe("ALMOST", () => {
       const filters = {
         mintProgress_eq: "ALMOST",
